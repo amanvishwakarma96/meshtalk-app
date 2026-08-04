47462840044c4535136f90ee717b63a92eb853fb
amanvishwakarma96 <80578610+amanvishwakarma96@users.noreply.github.com>
2026-08-04 17:18:03 +0530

    refactor: define durable message store contract

--- lib/core/storage/message_store.dart
+++ lib/core/storage/message_store.dart
@@ -1,8 +1,9 @@
-import 'package:meshtalk_app/core/ble/message_envelope.dart';
+import 'package:meshtalk_app/core/storage/stored_chat_message.dart';
 
 abstract interface class MessageStore {
-  Stream<List<MessageEnvelope>> watchRoom(String roomId);
-  Future<void> save(MessageEnvelope message);
-  Future<List<MessageEnvelope>> loadPendingOutbound();
+  Future<List<StoredChatMessage>> loadRoom(String roomId);
+  Future<List<StoredChatMessage>> loadPendingOutbound();
+  Future<void> upsert(StoredChatMessage message);
   Future<void> markSent(String messageId);
+  Future<void> close();
 }
