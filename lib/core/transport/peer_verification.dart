a642e2d05ca4a1426b3d0042866ed815fb6bfd1a
amanvishwakarma96 <80578610+amanvishwakarma96@users.noreply.github.com>
2026-08-05 14:31:17 +0530

    add peer verification contract

---
+++ lib/core/transport/peer_verification.dart
@@ -0,0 +1,23 @@
+class PeerVerificationRequest {
+  const PeerVerificationRequest({
+    required this.endpointId,
+    required this.peerId,
+    required this.displayName,
+    required this.authenticationToken,
+    required this.isIncomingConnection,
+  });
+
+  final String endpointId;
+  final String peerId;
+  final String displayName;
+  final String authenticationToken;
+  final bool isIncomingConnection;
+}
+
+abstract interface class PeerVerificationTransport {
+  List<PeerVerificationRequest> get currentVerificationRequests;
+  Stream<List<PeerVerificationRequest>> get verificationRequests;
+
+  Future<void> approvePeer(String endpointId);
+  Future<void> rejectPeer(String endpointId);
+}
